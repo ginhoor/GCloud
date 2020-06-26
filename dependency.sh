ed741be0cfbb58d6e81b0b3b137d248c6682a8a4
大帅 <ginhoor@gmail.com>
2020-06-26 11:19:52 +0800

    feat：修改依赖脚本

--- dependency.sh
+++ dependency.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip3 install git+https://github.com/ginhoor/charon_pluto.git@master --upgrade
-pip3 install git+https://github.com/ginhoor/charon_model.git@master --upgrade
+# pip3 install git+https://github.com/ginhoor/charon_model.git@master --upgrade
 pip3 install request
