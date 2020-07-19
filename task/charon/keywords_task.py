903174e36c6166d720666e17fe5a5713125dffc2
大帅 <ginhoor@gmail.com>
2020-07-19 20:49:15 +0800

    feat：更新关键字脚本

--- task/charon/keywords_task.py
+++ task/charon/keywords_task.py
@@ -4,7 +4,7 @@
 from charon_pluto.extension import cp_time
 import sys,time,requests, os
 
-#host = "http://127.0.0.1:12306"
+# host = "http://127.0.0.1:12306"
 host = "http://172.66.1.10:12306"
 
 def start_create_request(service, date=None):
@@ -13,8 +13,12 @@ def start_create_request(service, date=None):
         yestoday = today - 60*60*24
         date = cp_time.cp_timestamp_to_date(yestoday, "%Y-%m-%d")
     
-    path="keywords/{}/create".format(service)
-    params="date={}".format(date)
+    if service == 'analyze':
+        path = "keywords/{}/frequency".format(service)
+        params = "date={}".format(date)
+    else:
+        path = "keywords/{}/create".format(service)
+        params = "date={}".format(date)
 
     url = "{}/{}?{}".format(host, path, params)
     print(url)
@@ -49,13 +53,13 @@ if __name__ == "__main__":
     fetch_type = int(sys.argv[1])
     if fetch_type == 1:
         # 分析昨天关键字
-        service_list = ["news", "economy"]
+        service_list = ["news", "economy", "analyze"]
         for service in service_list:
             start_create_request(service)
 
     elif fetch_type == 2:
         # 分析当天关键字
-        service_list = ["news", "economy"]
+        service_list = ["news", "economy", "analyze"]
         for service in service_list:
             today = cp_time.cp_now_to_timestamp("%Y-%m-%d")
             start_create_request(service, today)
@@ -63,16 +67,19 @@ if __name__ == "__main__":
     elif fetch_type == 3:
         day_size = int(sys.argv[2])
         # 分析截止今天的x天时间，专业新闻
-        service_list = ["news", "economy"]
+        service_list = ["news", "economy", "analyze"]
         create_recently_data(day_size, service_list)
+
     elif fetch_type == 4:
-        day_size = int(sys.argv[2])
-        # 分析截止今天的x天时间，社会新闻
-        service_list = ["society"]
-        create_recently_data(day_size, service_list)
-    elif fetch_type == 5:
         start_date = sys.argv[2]
         day_size = int(sys.argv[3])
         # 分析一段时间，社会新闻
-        service_list = ["news", "economy"]
+        service_list = ["news", "economy", "analyze"]
+        create_range_data(start_date, day_size, service_list)
+
+    elif fetch_type == 5:
+        start_date = sys.argv[2]
+        day_size = int(sys.argv[3])
+        # 分析一段时间关键字词频
+        service_list = ["analyze"]
         create_range_data(start_date, day_size, service_list)
